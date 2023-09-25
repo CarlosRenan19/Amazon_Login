82c401a7a1442e91c0440d7d64b86aae51353a7b
Renan <r3n4ngamer14@gmail.com>
2023-09-25 12:59:16 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,4 @@
+function openOptions() {
+    document.querySelector('.options-details').classList.toggle('effect');
+    document.querySelector('.material-symbols-outlined').classList.toggle('rotateEffect')
+}
